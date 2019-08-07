f5796aeb6a8ce63be733b359b5a295dbf3816645
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-08-07 21:58:39 +0400

    commit 8.2

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 
+import SwapiService from '../../services/swapi-service';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import PeoplePage from '../people-page';
 import ErrorButton from '../error-button';
 import ErrorIndicator from '../error-indicator';
+import ItemList from '../item-list/item-list';
+import PersonDetails from '../person-details/person-details';
 
 import './app.css';
 
 export default class App extends Component {
+
+  swapiService = new SwapiService()
 
   state = {
     showRandomPlanet: true,
@@ -52,6 +57,28 @@ export default class App extends Component {
         </div>
   
         <PeoplePage />
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList 
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}
+            />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList 
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships}
+            />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
       </div>
     );
   }
